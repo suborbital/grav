759a7f11703ad08593e971c0b7a0a701a268c893
cohix <connorjhicks@gmail.com>
2020-08-20 21:30:38 -0400

    add second failure test

--- grav/pod_test.go
+++ grav/pod_test.go
@@ -170,6 +170,106 @@ func TestPodFailure(t *testing.T) {
 	}
 }
 
+func TestPodFailurePart2(t *testing.T) {
+	// test where the "bad" pod is somewhere in the "middle" of the ring
+	g := New()
+
+	counter := make(chan bool, 1000)
+
+	// and another 9 that don't
+	for i := 0; i < 4; i++ {
+		p2 := g.Connect()
+
+		p2.On(func(msg Message) error {
+			counter <- true
+
+			return nil
+		})
+	}
+
+	// create one pod that returns errors on "bad" messages
+	p := g.Connect()
+	p.On(func(msg Message) error {
+		counter <- true
+
+		if msg.Type() == msgTypeBad {
+			return errors.New("bad message")
+		}
+
+		return nil
+	})
+
+	// and another 9 that don't
+	for i := 0; i < 5; i++ {
+		p2 := g.Connect()
+
+		p2.On(func(msg Message) error {
+			counter <- true
+
+			return nil
+		})
+	}
+
+	pod := g.Connect()
+
+	// send 64 "bad" messages (64 reaches the highwater mark)
+	for i := 0; i < 64; i++ {
+		pod.Send(NewMsg(msgTypeBad, []byte(fmt.Sprintf("hello, world %d", i))))
+	}
+
+	time.Sleep(time.Duration(time.Second))
+
+	// send 10 more "bad" messages
+	for i := 0; i < 10; i++ {
+		pod.Send(NewMsg(msgTypeBad, []byte(fmt.Sprintf("hello, world %d", i))))
+	}
+
+	time.Sleep(time.Duration(time.Second))
+
+	count := 0
+	more := true
+	for more {
+		select {
+		case <-counter:
+			count++
+		default:
+			more = false
+		}
+	}
+
+	// 730 because the 64th message to the "bad" pod put it over the highwater
+	// mark and so the last 10 message would never be delievered
+	// sleeps were needed to allow all of the internal goroutines to finish execing
+	// in the worst case scenario of a single process machine (which lots of containers are)
+	if count != 730 {
+		t.Errorf("incorrect number of messages, expected 730, got %d", count)
+	}
+
+	// the first pod should now have been disconnected, causing only 9 recievers reset and test again
+
+	// send 10 "normal" messages
+	for i := 0; i < 10; i++ {
+		pod.Send(NewMsg(MsgTypeDefault, []byte(fmt.Sprintf("hello, world %d", i))))
+	}
+
+	time.Sleep(time.Duration(time.Second))
+
+	count = 0
+	more = true
+	for more {
+		select {
+		case <-counter:
+			count++
+		default:
+			more = false
+		}
+	}
+
+	if count != 90 {
+		t.Errorf("incorrect number of messages, expected 90, got %d", count)
+	}
+}
+
 func TestPodFlushFailed(t *testing.T) {
 	g := New()
 
